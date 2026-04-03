5b5b7670eba873706bb4c90f7749e92b379eb8ee
dnguyen227 <d227nguy@uwaterloo.ca>
2026-04-02 23:37:49 -0400

    .

--- src/utilities.jl
+++ src/utilities.jl
@@ -9,42 +9,6 @@ function _copy_model(
 end
 
 """
-    copy_model_with_constraints(model, constraints, method)
-
-Create a new model with only the variables (and their bounds)
-from `model` and the selected `constraints`. Returns a
-`GDPSubmodel` wrapping the new model.
-"""
-function copy_model_with_constraints(
-    model::JuMP.AbstractModel,
-    constraints::Vector{<:DisjunctConstraintRef},
-    method::_MBM
-    )
-    var_type = JuMP.variable_ref_type(model)
-    sub_model = _copy_model(model)
-    decision_vars = collect_all_vars(model)
-    fwd_map = Dict{var_type, Vector{var_type}}()
-
-    for var in decision_vars
-        copy_var = variable_copy(sub_model, var)
-        fwd_map[var] = [copy_var]
-    end
-
-    for cref in constraints
-        con = JuMP.constraint_object(cref)
-        flat_map = Dict(v => only(ws) for (v, ws) in fwd_map)
-        expr = _replace_variables_in_constraint(con.func, flat_map)
-        T = one(JuMP.value_type(typeof(sub_model)))
-        JuMP.@constraint(sub_model, expr * T in con.set)
-    end
-
-    JuMP.set_optimizer(sub_model, method.optimizer)
-    JuMP.set_silent(sub_model)
-
-    return GDPSubmodel(sub_model, decision_vars, fwd_map)
-end
-
-"""
     copy_and_reformulate(model, decision_vars, reform_method, method)
 
 Copy the GDP model, reformulate the copy with `reform_method`,
