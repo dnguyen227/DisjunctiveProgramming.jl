abc410428b5c64ea3e04af8cdcc4686bcee651d6
d227nguyen <d227nguy@uwaterloo.ca>
2026-08-17 13:22:25 -0400

    aqua revert

--- test/aqua.jl
+++ test/aqua.jl
@@ -1,7 +1,6 @@
 using Aqua
 using DisjunctiveProgramming
 
-Aqua.test_all(DisjunctiveProgramming, deps_compat = false,
-    ambiguities = false)
+Aqua.test_all(DisjunctiveProgramming, deps_compat = false, ambiguities = false)
 Aqua.test_deps_compat(DisjunctiveProgramming, check_extras = (ignore=[:Test, :HiGHS],))
 Aqua.test_ambiguities(DisjunctiveProgramming)
